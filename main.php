9ebccc340ebaa360a4bfc620d61b98c0f2ff77bb
Sérgio Vilar <sergiovilar.r@gmail.com>
2012-04-21 09:59:37 -0300

    Requires automáticos de arquivos de configuração, helpers, shortcodes, widgets e plugins

--- main.php
+++ main.php
@@ -15,29 +15,28 @@ define(IMAGES_URL,get_bloginfo('template_url')."/images/");
 define(CSS_URL,FEELSEN_URL."css/");
 define(JS_URL,FEELSEN_URL."js/");
 
-/* Includes */
-require('inc/core-functions.php');
-require('inc/wordpress-mods.php');
-require('inc/template-tags.php');
+function include_all_php($folder){
 
-/* Helpers */
-require('core/helpers/html.class.php');
-require('core/helpers/form.class.php');
-require('core/helpers/social.class.php');
+	$folder = get_template_directory()."/turumim/".$folder;
+    foreach (glob("{$folder}/*.php") as $filename):
+		require $filename;
+    endforeach;
+    
+}
 
-/* Shortcodes */
-require('core/shortcodes/social.class.php');
-/* Widgets */
+/* Requires */
+include_all_php("inc");
+include_all_php("core/helpers");
+include_all_php("core/shortcodes");
+include_all_php("core/widgets");
+include_all_php("plugins");
 
-/* Plugins */
-require('plugins/contact-form.class.php');
 
 /* Inicia os Helpers */
 $html = new HtmlHelper();
 $form = new FormHelper();
 $social = new SocialHelper();
 
-/* Inicia os Shortcodes */
-$socialsh = new SocialShortcodes();
+/* Inicia os Shortcodes */$socialsh = new SocialShortcodes();
 
 ?>
